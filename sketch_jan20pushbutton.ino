0b1f76c5ac8a5fbb257c992bbb201904e8ed5edf
cahoward123 <caiden-howard@student.kirkwood.edu>
2026-01-20 09:34:22 -0600

    Add files via upload

---
+++ sketch_jan20pushbutton.ino
@@ -0,0 +1,20 @@
+// Listing 4-1, Project 4 - Demonstrating a Digital Input
+ #define LED 3 
+#define BUTTON 7
+
+void setup()
+{
+   pinMode(LED, OUTPUT);   // output for the LED 
+   pinMode(BUTTON, INPUT); // input for the button
+}
+ 
+void loop()
+{
+   if ( digitalRead(BUTTON) == HIGH )
+   {
+      digitalWrite(LED, HIGH); // turn on the LED
+      delay(500);              // wait for 0.5 seconds
+      digitalWrite(LED, LOW);  // turn off the LED
+     
+   }
+}
